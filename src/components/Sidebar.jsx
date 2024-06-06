83c1800bd9dbb1de79b1a67c1a484783d5cd627a
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-06 19:19:45 +0300

    Sidebar component creation

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <button>
+
+      </button>
+      <button>
+
+      </button>
+      <nav>
+        <ul>
+            <li>Investments</li>
+            <li>Settings</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+export default Sidebar;
