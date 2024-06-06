246a60f7cefad552a89949a9c45e0929020a9ea8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-06-07 00:46:13 +0300

    Update Investments.jsx

--- src/pages/Investments.jsx
+++ src/pages/Investments.jsx
@@ -1,8 +1,3 @@
-import { useEffect, useState } from "react"; 
-import axios from "axios";
-import HighchartsReact from "highcharts-react-official";
-import Highcharts from 'highcharts';
-
 const Investments = () =>{
 
 
